dc125f7263adcef31a47ec929d7da5879319e55f
JoaoDiasMorais <116293811+JoaoDiasMorais@users.noreply.github.com>
2025-02-03 17:11:36 +0000

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,20 @@
+alunos1 = float(input("Qual é a nota do aluno 1 "))
+alunos2 = float(input("Qual é a nota do aluno 2 "))
+alunos3 = float(input("Qual é a nota do aluno 3 "))
+alunos4 = float(input("Qual é a nota do aluno 4 "))
+alunos5 = float(input("Qual é a nota do aluno 5 "))
+alunos6 = float(input("Qual é a nota do aluno 6 "))
+print(f"nota total {alunos5+alunos6+alunos1+alunos3+alunos4+alunos2}")
+print(f"média de alunos da turma 1{total/6}")
+alunos7 = float(input("Qual é a nota do aluno 7 "))
+alunos8 = float(input("Qual é a nota do aluno 8 "))
+alunos9 = float(input("Qual é a nota do aluno 9 "))
+alunos10 = float(input("Qual é a nota do aluno 10 "))
+alunos11 = float(input("Qual é a nota do aluno 11 "))
+alunos12 = float(input("Qual é a nota do aluno 12 "))
+print(f"nota total {alunos7+alunos8+alunos9+alunos10+alunos11+alunos12}")
+print(f"média de alunos da turma 2{total2/6}")
+
+
+
+
