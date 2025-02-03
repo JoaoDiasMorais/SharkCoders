ee79159e718d075fe2433cb89378824aa07d0047
JoaoDiasMorais <116293811+JoaoDiasMorais@users.noreply.github.com>
2025-02-03 18:09:50 +0000

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,9 @@
+nota = float(input("Primeira nota"))
+nota2 = float(input("Segunda nota"))
+media = nota + nota2
+media2 = media/2
+print(f"média {media2}")
+if media2 >=60:
+    print("Parabéns!Aluno aprovado")
+else:
+    print("Aluno reprovado")
