eb9d0d0caceb4d95e08ea4ac5997ea062ad80027
JoaoDiasMorais <116293811+JoaoDiasMorais@users.noreply.github.com>
2025-02-10 17:55:01 +0000

    Add files via upload

---
+++ Aula6.2.py
@@ -0,0 +1,11 @@
+idade = int(input("Qual é a tua idade?"))
+if idade <= 9:
+    print("És um minir")
+elif idade <= 14:
+    print("És um infantil")
+elif idade <= 19:
+    print("És um junior")
+elif idade <= 25:
+    print("És um senior")
+else:
+    print("És um master")
