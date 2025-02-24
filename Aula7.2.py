a145cac85db209df70c9ce400299e3dda101f3d1
JoaoDiasMorais <116293811+JoaoDiasMorais@users.noreply.github.com>
2025-02-24 18:14:00 +0000

    Add files via upload

---
+++ Aula7.2.py
@@ -0,0 +1,7 @@
+X = int(input("Valor"))
+if X > 0:
+    for x in range(0,X):
+        print(x)
+if X < 0:
+    for x in range(X,0):
+        print(x)
---
+++ Aula7.py
@@ -0,0 +1,12 @@
+I = int(input("Valor para i"))
+i = I
+while  i>-1:
+    print(i)
+    i =i-1
+    if i == 0:
+        break
+while i<1:
+    print(i)
+    i = i+1
+    if i == 1:
+        break
