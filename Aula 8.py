88f7256da86eca60b22d20bd23de501620ab16b6
JoaoDiasMorais <116293811+JoaoDiasMorais@users.noreply.github.com>
2025-03-17 18:18:33 +0000

    Add files via upload

---
+++ Aula 8.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Escolhe um numero "))
+numero2 = int(input("Escolhe outro numero "))
+par_ou_impar = str(input("Par ou Impar?"))
+if numero1 %:
+    for x in range(numero1,numero2 + 1,2):
+        print(x)
