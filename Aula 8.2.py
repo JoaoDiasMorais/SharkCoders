c4b9ee20d92b4ec12fe88b59854f059fa5696553
JoaoDiasMorais <116293811+JoaoDiasMorais@users.noreply.github.com>
2025-03-24 17:40:01 +0000

    Add files via upload

---
+++ Aula 8.2.py
@@ -0,0 +1,19 @@
+numero1 = int(input("Escolhe um numero "))
+numero2 = int(input("Escolhe outro numero "))
+par_ou_impar = str(input("par ou impar?"))
+if par_ou_impar == "par":
+    if numero1 % 2 == 0:
+        if (numero2 + 1) % 2 == 0:
+            for x in range(numero1,numero2 + 1, 2):
+                print(x)
+        else:
+            for x in range(numero1, numero2 + 1, 2):
+                print(x)
+else:
+     if (numero2 + 1) % 2 == 0:
+         for x in range(numero1, numero2 + 1, 2):
+            print(x)
+            print(numero1 + numero2)
+     else:
+        for x in range(numero1, numero2 + 1, 2):
+            print(x)
