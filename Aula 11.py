b540eabbedccac23b1cb3f58ee0be2d2ed17e712
JoaoDiasMorais <116293811+JoaoDiasMorais@users.noreply.github.com>
2025-04-07 20:46:14 +0100

    Update Aula 11.py

--- Aula 11.py
+++ Aula 11.py
@@ -2,25 +2,36 @@ def sum(numeros):
     total = 0
     for x in numeros:
         total += x
-        return(total)
+    return(total)
 
 def sub(numeros):
-    total = 0
-    for x in numeros:
+    total = numeros[0]
+    for x in numeros[1:]:
         total -= x
-        return(total)
+    return(total)
 
 def mult(numeros):
     total = 1
     for x in numeros:
         total *= x
-        return(total)
+    return(total)
 
 def div(numeros):
-    total = 1
-    for x in numeros:
+    total = numeros[0]
+    for x in numeros[1:]:
         total /= x
-        return(total)
-        print(total)
+    return(total)
 
-div()
+primeiro = int(input("Escolhe um numero para ser mudado na operacao: "))
+segundo = int(input("Escolhe um numero para mudar o primeiro numero na operacao: "))
+operacao = str(input("Escolhe a operacao: "))
+
+if operacao == "mult":
+    print(mult([primeiro,segundo]))
+elif operacao == "sub":
+    print(sub([primeiro,segundo]))
+elif operacao == "div":
+    print(div([primeiro,segundo]))
+else:
+    print(sum([primeiro,segundo]))
+
